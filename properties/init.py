b4b973ccd55fce8e08f943f32db6d939b9d89742
Sebastian Ozimkowski <sebastianozimkowski@outlook.com>
2021-05-11 18:21:00 +0200

    v3

--- properties/init.py
+++ properties/init.py
@@ -3,6 +3,7 @@ from requests_html import HTMLSession
 
 os.system("python3 manage.py makemigrations")
 os.system("python3 manage.py migrate")
+os.system("python3 manage.py collectstatic")
 
 session = HTMLSession()
 r = session.get("http://1.1.1.1")
